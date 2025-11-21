24546d6a93f25fe5baa02c3a10dc7a0739447956
rahmisyaf <rahmisyafitri261@gmail.com>
2025-11-22 01:49:54 +0800

    style: moods button interactivity

---
+++ src/components/mood-com/MoodButtonLeft.vue
@@ -0,0 +1,17 @@
+<script setup>
+  import '../../styles/mood-and-activity.css'
+  defineProps({
+    imgUrl: String,
+    bgColor: String,
+    moodName: String
+  })
+</script>
+
+<template>
+  <div class="mood-button-left">
+    <div :class="[bgColor]" class="mood-icon-bg">
+      <img :src="imgUrl" class="h-12 w-12">
+    </div>
+    <p>{{ moodName }}</p>
+  </div>
+</template>
